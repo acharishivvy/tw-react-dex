0adc0e8674377d05480af88b8aca49a682a5c002
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2021-06-09 20:46:11 +1200

    Various Changes.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 import axios from "axios";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
   let pokeAPI = "https://pokeapi.co/api/v2/pokemon?limit=12";
   let [pokemonList, setPokemonList] = useState([]);
-  let [next, setNext] = useState("");
-  let [prev, setPrev] = useState("");
+  let [next, setNext] = useState(null);
+  let [prev, setPrev] = useState(null);
 
+  // ! Probably the most important function, needs to be more robust
   const getPokemon = (url) => {
     axios
       .get(url)
@@ -26,23 +26,62 @@ function App() {
     console.log(pokemonList, next, prev);
   };
 
-  useEffect(() => {
+  // ? Update Name here, simple pagination
+  const paginate = (napi) => {
+    if (napi === null){
+      alert("No More Pokemon Available")
+    } else {
+      console.log(napi)
+      getPokemon(napi)
+    }
+  }
+
+  // const searchPokemon = (query) => {
+
+  // }
+
+  useLayoutEffect(() => {
     getPokemon(pokeAPI);
-  }, [pokeAPI]);
+  }, []);
 
   return (
     <>
-      <div className="App container p-6 mx-auto">
-        <h1 className="text-3xl font-medium tracking-wide text-gray-800 dark:text-white md:text-4xl">
-          Simple AF Pokedex
-        </h1>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          onClick={() => getPokemon(pokeAPI)}
-        >
-          Refetch
-        </button>
+      <div class="w-full h-screen bg-center bg-no-repeat bg-cover">
+        <div class="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center">
+          <div class="mx-4 text-center text-white">
+            <h1 class="font-bold text-6xl mb-4">React Pokedex</h1>
+            <h2 class="font-bold text-3xl mb-12">don't expect too much</h2>
+            <div>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => getPokemon(pokeAPI)}
+              >
+                Refetch
+              </button>
+
+              <ul>
+                {pokemonList.map((pkm) => (
+                  <li key={pkm.id}>{pkm.name}</li>
+                ))}
+              </ul>
+
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => paginate(prev)}
+              >
+                Prev
+              </button>
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                onClick={() => paginate(next)}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+
       <PokemonCard />
     </>
   );
---
+++ src/components/PokemonCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PokemonCard() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default PokemonCard;
