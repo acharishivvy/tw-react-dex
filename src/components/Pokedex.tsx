9df5ae5bc0744fdef2d6d8dea7f9eea86978ffb1
Shivneel Achari <22414863+SaiyanShivvy@users.noreply.github.com>
2025-03-13 00:57:07 +1300

    Update Pokedex.tsx

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -1,21 +1,19 @@
 import React, { useState } from "react";
-import { PokemonClient } from "pokenode-ts";
+import { MainClient, PokemonClient } from "pokenode-ts";
 
 export default function Pokedex() {
   const [pokedex, SetPokedex] = useState<any[]>([]);
+  const api = new MainClient();
 
-  (async () => {
-    const api = new PokemonClient();
+  //await api.listPokemons(0, 2000).then((data) => SetPokedex(data.results));
 
-    await api.listPokemons(0, 2000).then((data) => SetPokedex(data.results));
-  })();
   return (
     <div>
       <h1>User List</h1>
       <ul>
-        {pokedex.map((pokemon) => (
+        {/* {pokedex.map((pokemon) => (
           <li key={pokemon.url}>{pokemon.name}</li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
